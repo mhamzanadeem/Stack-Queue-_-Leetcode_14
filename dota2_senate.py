94c393b940bf1f93a522fc4d139fc8395c91c48a
Hamza Nadeem <nadeemmhamza@gmail.com>
2026-08-26 10:08:58 +0500

    feat: Dota2 Senate

---
+++ dota2_senate.py
@@ -0,0 +1,30 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        n = len(senate)
+
+        radiant = deque()
+        dire = deque()
+
+        # Store indices of each party
+        for i, ch in enumerate(senate):
+            if ch == "R":
+                radiant.append(i)
+            else:
+                dire.append(i)
+
+        while radiant and dire:
+
+            r = radiant.popleft()
+            d = dire.popleft()
+
+            if r < d:
+                # Radiant acts first and bans Dire
+                radiant.append(r + n)
+            else:
+                # Dire acts first and bans Radiant
+                dire.append(d + n)
+
+        if radiant:
+            return "Radiant"
+        else:
+            return "Dire"
